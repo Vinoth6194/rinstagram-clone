275d7cc7e2ec869cf7d8169ffac46b091b6cbec8
vin-unicity <vinoth.kumar@unicity.com>
2020-08-15 19:27:07 +0530

    logo displayed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-     <h1>Instagram Clone | Vinothkumar S 🔥 🔥 🔥 🔥 </h1>
+    <div className="app">
+      <div className="app__header">
+        <img
+          className="app_headerImage"
+          src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+          alt="Instagram"
+        ></img>
+      </div>
+      <h1>Instagram Clone | Vinothkumar S 🔥 🔥 🔥 </h1>
     </div>
   );
 }
