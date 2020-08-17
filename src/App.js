d2c4415ca5a57b1552365573c85ee804751e2c73
vin-unicity <vinoth.kumar@unicity.com>
2020-08-17 17:59:16 +0530

    signup modal

--- src/App.js
+++ src/App.js
@@ -2,36 +2,87 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import Post from "./Post";
 import { db } from "./Firebase";
+import { makeStyles } from "@material-ui/core/styles";
+import Modal from "@material-ui/core/Modal";
+import { Button, Input } from "@material-ui/core";
 
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
 function App() {
-  const [posts, setPosts] = useState([
-    // {
-    // username  : "vinoth_6194" ,
-    // caption :"I am pumed up" ,
-    // imageUrl :"https://i.morioh.com/200623/6c839150.jpg"
-    // },
-    // {
-    //   username : "chudar_619" ,
-    //   caption : "Me too" ,
-    //   imageUrl : "https://allvectorlogo.com/img/2017/10/facebook-messenger-logo.png"
-    // },
-    // {
-    //   username : "kiru_1849" ,
-    //   caption : "Yes yes yes" ,
-    //   imageUrl : "https://www.edigitalagency.com.au/wp-content/uploads/instagram-logo-svg-vector-for-print.svg"
-    // }
-  ]);
+  const classes = useStyles();
+
+  const [posts, setPosts] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [modalStyle] = useState(getModalStyle);
+  const [username,setUsername] = useState();
+  const [password,setPassword] = useState();
+  const [email,setEmail] = useState();
+
+  const signup = event =>{
+    
+  }
 
   useEffect(() => {
     db.collection("posts").onSnapshot((snapshot) => {
-      setPosts(snapshot.docs.map(doc => ({
-        id:doc.id,
-        post:doc.data()
-      })));
+      setPosts(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          post: doc.data(),
+        }))
+      );
     });
   }, []);
   return (
     <div className="app">
+      <Modal open={open} onClose={() => setOpen(false)}>
+        <div style={modalStyle} className={classes.paper}>
+          <center>
+            <img
+              className="app_headerImage"
+              src="https://www.instagram.com/static/images/web/mobile_nav_type_logo.png/735145cfe0a4.png"
+              alt="Instagram"
+            ></img>
+            <Input
+              placeholder="Your Name"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            ></Input>
+            <Input
+              placeholder="Your Email"
+              type="text"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            ></Input>
+            <Input 
+            placeholder ="Your Password"
+            type = "text"
+            value ={password}
+            onChange ={(e)=>setPassword(e.target.value)}>
+            </Input>
+            <Button onClick={signup}>Sign Up</Button>
+          </center>
+        </div>
+      </Modal>
       <div className="app__header">
         <img
           className="app_headerImage"
@@ -39,10 +90,11 @@ function App() {
           alt="Instagram"
         ></img>
       </div>
+      <Button onClick={() => setOpen(true)}>Sign Up</Button>
       <h1>Instagram Clone | Vinothkumar S 🔥 🔥 🔥 </h1>
-      {posts.map(({id,post}) => (
+      {posts.map(({ id, post }) => (
         <Post
-          key ={id}
+          key={id}
           username={post.username}
           caption={post.caption}
           imageUrl={post.imageUrl}
